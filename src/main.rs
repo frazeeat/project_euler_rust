a64c8cf9b752c99f8d8fec5afcff2cd5cc57c89d
Adam Frazee <frazeeat@miamioh.edu>
2019-10-19 09:17:34 -0400

    Completed the third project Euler problem.

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,9 @@ mod set_one;
 
 fn main() {
     let p_1 : i32 = set_one::problem_one(1000);
+    let p_2 : u32 = set_one::problem_two(4000000);
+    let p_3 : u64 = set_one::problem_three(600851475143);
     println!("Problem One: {}", p_1);
+    println!("Problem Two: {}", p_2);
+    println!("Problem Three: {}", p_3);
 }
--- src/set_one.rs
+++ src/set_one.rs
@@ -1,3 +1,25 @@
+struct Fibonacci{
+    curr: u32,
+    next: u32,
+}
+
+impl Iterator for Fibonacci{
+    type Item = u32;
+
+    fn next(&mut self)-> Option<u32>{
+        let new_next = self.curr + self.next;
+
+        self.curr = self.next;
+        self.next = new_next;
+
+        Some(self.curr)
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci{curr: 1, next: 1}
+}
+
 pub fn problem_one(number: i32)-> i32{
     let mut count: i32 = 0;
     for sub_number in 0..number{
@@ -7,3 +29,46 @@ pub fn problem_one(number: i32)-> i32{
     }
     return count;
 }
+
+pub fn problem_two(limit: u32)->u32{
+    let mut count: u32 = 0;
+    let mut fib = fibonacci();
+    while let Some(i) = fib.next(){
+        if (i % 2) == 0{
+            count+=i;
+        } else if i >= limit{
+            break
+        }
+    }
+    return count;
+}
+
+fn is_prime(input: u64)->bool{
+    if input <= 3{
+        return input > 1;
+    }else if input % 2 == 0 || input % 3 == 0 {
+        return false;
+    }else{
+        let mut i: u64 = 5;
+        while i * i <= input{
+            if input % i  == 0 || input % (i + 2) == 0{
+                return false;
+            }
+            i += 6;
+        }
+    }
+    return true;
+}
+
+pub fn problem_three(input: u64)->u64{
+    let fl_input = input as f64;
+    let sq_input = fl_input.sqrt();
+    let inte_sq_input = sq_input as u64;
+    for n in (1..inte_sq_input+1).rev(){
+        if is_prime(n) && input % n == 0{
+            return n;
+        } 
+    }
+    return 1;
+}
+
